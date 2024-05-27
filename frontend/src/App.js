2b2d096e11089e37448983257617547962f60f3e
Leandromarinh <leandrinm14@gmail.com>
2024-05-27 15:34:48 -0300

    frontend working but connetion with backend not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch(`http://${process.env.BACKEND_SERVICE_HOST}/goals`);
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch(`http://${process.env.BACKEND_SERVICE_HOST}/goals`, {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch(`http://${process.env.BACKEND_SERVICE_HOST}/goals/` + goalId, {
         method: 'DELETE',
       });
 
